32373bc8770e533d5945c176a2f23d339fcbb636
Shazzad_softzino <shazzad@softzino.com>
2025-10-27 14:46:10 +0600

    index and js update

---
+++ script.js
@@ -0,0 +1,60 @@
+// var lang = "English";
+
+// function learn(topic) { 
+//     var lang = topic;
+
+//     // ? js let block */
+//     // if(true){
+//     //     let a = 5;
+//     //     a = 7; 
+//     //     console.log(a);
+//     //}
+//     if(true){
+//         const a = {
+//             b: 5,
+//         };
+//         a.b = 8;
+//         console.log(a);
+//     }
+
+//     console.log(`I am learning ${topic}`);
+// };
+
+// learn("JavaScript");
+
+// console.log(`I Know ${lang}`);
+// js scope 
+
+// js function  
+// function hello () {
+//    console.log("Hello World");
+// };
+// let msg = hello();
+// // console.log(msg);
+
+// // js name expecition function 
+
+// const learning = function learning () {
+//     console.log("I am learning Javascript");
+// };
+
+// arrow Function 
+
+// const hello = () =>({
+//     a:4,
+//     b:6,
+// });
+// console.log(hello());
+
+// annoymous function 
+
+// function hello() {
+//     return () => {
+//         console.log("Hello World");
+//     };
+// }
+
+// hello()();
+
+// event handler
+ 
